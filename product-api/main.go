0ce610d8c4984074fa6d49ae24eba5a3b81cfc56
courtney l <courtneyl@pop-os.localdomain>
2020-06-16 20:35:02 -0400

    end of video three

--- product-api/main.go
+++ product-api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
-	"os"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/clandau/microsvcs-in-go/product-api/handlers"
 )
 
@@ -16,5 +20,30 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan:= make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("recieved terminate, graceful shutdown", sig)
+
+	
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 	http.ListenAndServe(":9090", sm)
 }
