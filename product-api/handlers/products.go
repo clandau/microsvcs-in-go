8c3162740818da5e800e5687aed329394e7a2af2
courtney l <clandau@learnics.com>
2020-07-05 16:01:02 -0400

    done with video 3

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -15,6 +15,22 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle get request
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+
+	// handle an update
+	if r.Method == http.MethodPut {
+		
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
